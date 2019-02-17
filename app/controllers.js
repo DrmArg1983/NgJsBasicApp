287909a77afba72d8dc5e821c7a72bae1b19323f
diego moreno <diegomoreno2004@gmail.com>
2019-02-17 16:03:20 +0100

    Implemented get for user in the controller and users view in template

--- app/controllers.js
+++ app/controllers.js
@@ -1,12 +1,17 @@
 angular
   .module("app")
   .controller("MainController", function($scope, $resource) {
+    
     Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {
       id: "@id"
     });
 
-    $scope.posts = Post.query(); //query returns [] by default
+    User = $resource("http://jsonplaceholder.typicode.com/users/:id", {
+      id: "@id"
+    });
 
+    $scope.posts = Post.query(); //query returns [] by default
+    $scope.users = User.query(); //query returns [] by default
   })
   .controller("HomeController", function() {
     console.log("2. HomeController loaded!");
