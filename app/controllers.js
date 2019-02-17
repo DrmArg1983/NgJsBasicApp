19f4a4febbb6fbe4a37612dac7ea076b85387bb1
diego moreno <diegomoreno2004@gmail.com>
2019-02-17 19:48:54 +0100

    Implemented delete for posts

--- app/controllers.js
+++ app/controllers.js
@@ -1,7 +1,6 @@
 angular
   .module("app")
   .controller("MainController", function($scope, $resource) {
-    
     Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {
       id: "@id"
     });
@@ -12,13 +11,21 @@ angular
 
     $scope.posts = Post.query(); //query returns [] by default
     $scope.users = User.query(); //query returns [] by default
+    $scope.removePost = function(post) {
+      Post.delete({id: post.id });
+        //this is because API is Fake, Query should be uses in real API.
+      $scope.posts= $scope.posts.filter(function(element){
+        return element.id !== post.id;
+      })
+
+    };
   })
   .controller("PostController", function($scope, $resource, $routeParams) {
     Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {
       id: "@id"
     });
 
-    $scope.post= Post.get({id:$routeParams.id});
+    $scope.post = Post.get({ id: $routeParams.id });
   })
   .controller("AboutController", function() {
     console.log("3. AboutController loaded!");
