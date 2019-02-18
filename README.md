64cb04e2525098364fe9f5136804895f2d0726c2
diego moreno <diegomoreno2004@gmail.com>
2019-02-18 10:49:35 +0100

    Implemented some basic e2e tests

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NgJsBasicApp
-AngularJs basic application
+AngularJs basic application with basic e2e tests
+
+start application: npm start
+
+Testing:
+
+webdriver-manager start
+then
+You can see information about the status of the server at http://localhost:4444/wd/hub.
+then
+run the tests with : protractor conf.js
---
+++ tests/todo-spec.js
@@ -0,0 +1,46 @@
+describe("NGCRUD App", function() {
+  it("should have a title", function() {
+    browser.get("http://localhost:8080/#/");
+
+    var lnkStart = element(by.id('lnkStart'));
+    var lnkCreatePost = element(by.id('lnkCreatePost'));
+
+    expect(lnkStart.getText()).toEqual('Start');
+    expect(lnkCreatePost.getText()).toEqual('Create Post');
+  });
+
+  it("should have basic navigation from index", function() {
+    browser.get("http://localhost:8080/#/");
+
+    var lnkStart = element(by.id('lnkStart'));
+    var lnkCreatePost = element(by.id('lnkCreatePost'));
+
+    expect(lnkStart.getText()).toEqual('Start');
+    expect(lnkCreatePost.getText()).toEqual('Create Post');
+  });
+
+  it("should have loaded tab control view", function() {
+    browser.get("http://localhost:8080/#/");
+
+    var basicActions='READ MORE EDIT DELETE';
+    var textFromFirstItem= 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit';
+    var history = element.all(by.repeater('post in posts'));
+    
+    expect(history.count()).toEqual(100);
+    expect(history.first().getText()).toContain(textFromFirstItem);
+    expect(history.first().getText()).toContain(basicActions);
+  });
+  
+  it("should load a single post", function() {
+    browser.get("http://localhost:8080/#/");
+
+    var history = element.all(by.repeater('post in posts'));
+    var btnReadMore=history.first().all(by.css(".btn--blue"));
+    btnReadMore.click();
+
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/#/post/1");
+    
+  });
+
+
+});
